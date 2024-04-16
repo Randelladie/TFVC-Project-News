72927e12437193028e0f8e5d04a03c46d891446f
Randelladie <100122826+Randelladie@users.noreply.github.com>
2024-04-16 19:04:56 +0800

    Changes
    made a layout page master

---
+++ Files/News/Files/mainRes/main.js
@@ -0,0 +1,9 @@
+function ToggleCategory() {
+    var content = document.getElementById("Main_AsideCategory");
+    //this.classList.toggle("active");
+    if (content.style.display === "block") {
+    content.style.display = "none";
+    } else {
+    content.style.display = "block";
+    }
+}
